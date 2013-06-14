42c4322d939039c2d37b7f16882420ae0c954b63
Chau Tran <chauttm.vnu@gmail.com>
2013-06-14 08:40:54 +0700

    source code added read pom.xml for list of dependencies

---
+++ src/main/java/SavingsAccountDAO.java
@@ -0,0 +1,32 @@
+import org.dbunit.database.IDatabaseConnection;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chauttm
+ * Date: 6/13/13
+ * Time: 3:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SavingsAccountDAO {
+    private Connection dbConnection;
+
+    public SavingsAccountDAO(DataSource dataSource) throws SQLException {
+        this.dbConnection = dataSource.getConnection();
+    }
+
+    public SavingsAccount findByAccountNumber(String accountNumber) throws SQLException {
+        String queryString = "SELECT * FROM SAVINGS_ACCOUNT WHERE ACCOUNT_NUMBER='" + accountNumber + "'";
+        ResultSet resultSet = dbConnection.createStatement().executeQuery(queryString);
+        if(resultSet.next())
+            return new SavingsAccount(accountNumber, resultSet.getString("description"),resultSet.getDouble("balance"));
+        else
+            return null;
+    }
+}
+
---
+++ src/test/java/TestSavingsAccountDAO.java
@@ -0,0 +1,77 @@
+import org.dbunit.IDatabaseTester;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ReplacementDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+import org.h2.jdbcx.JdbcDataSource;
+import org.h2.tools.RunScript;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.sql.Connection;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class TestSavingsAccountDAO {
+    private static final String JDBC_DRIVER = org.h2.Driver.class.getName();
+    private static final String JDBC_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "";
+
+    private Connection dbConnection;
+    private SavingsAccountDAO savingsAccountDAO;
+
+    @BeforeClass
+    public static void createSchema() throws Exception {
+        String schemaFileName = System.class.getResource("/schema.sql").toString().substring(6);
+        RunScript.execute(JDBC_URL, USER, PASSWORD, schemaFileName, Charset.forName("UTF8"), false);
+    }
+
+    @Before
+    public void importDataSet() throws Exception {
+        IDataSet dataSet = readDataSet();
+        cleanlyInsert(dataSet);
+    }
+
+    private IDataSet readDataSet() throws Exception {
+        return new FlatXmlDataSetBuilder().build(System.class.getResource("/user.xml"));
+    }
+
+    private void cleanlyInsert(IDataSet dataSet) throws Exception {
+        IDatabaseTester databaseTester = new JdbcDatabaseTester(JDBC_DRIVER, JDBC_URL, USER, PASSWORD);
+        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+        databaseTester.setDataSet(dataSet);
+        databaseTester.onSetup();
+    }
+
+    @Test
+    public void testFindByAccountNumber() throws Exception {
+        SavingsAccountDAO savingsAccountDAO = new SavingsAccountDAO(dataSource());
+        SavingsAccount account = savingsAccountDAO.findByAccountNumber("0123456789");
+
+        assertEquals("0123456789", account.getAccountNumber());
+    }
+
+    private void insertDataSetIntoDb(ReplacementDataSet replacementDataSet)  throws Exception{
+        DatabaseOperation.CLEAN_INSERT.execute(
+                new DatabaseConnection(dbConnection), replacementDataSet);
+    }
+    private DataSource dataSource() {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setURL(JDBC_URL);
+        dataSource.setUser(USER);
+        dataSource.setPassword(PASSWORD);
+        return dataSource;
+    }
+}
+
